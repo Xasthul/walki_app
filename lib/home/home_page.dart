65c3dd8632e1c2d961830e3718cb9216e2d95daf
nazarii <nazarii.honcharenko@haven.com>
2024-07-06 20:37:28 +0300

    Add google maps

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,12 +1,29 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) => const Scaffold(
-        body: Center(
-          child: Text('Home'),
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: _controller.complete,
+          compassEnabled: false,
         ),
       );
 }
