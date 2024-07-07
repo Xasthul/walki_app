3ecb6a666e5400d78bdc19db4947200de1c24736
nazarii <nazarii.honcharenko@haven.com>
2024-07-07 16:00:56 +0300

    Add app_dependencies.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vall/app/app_dependencies.dart';
 import 'package:vall/app/routes/app_routes.dart';
 
 void main() {
@@ -9,13 +10,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        title: 'Vall',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+  Widget build(BuildContext context) => AppDependencies(
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'Vall',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          routerConfig: appRoutes,
         ),
-        routerConfig: appRoutes,
       );
 }
