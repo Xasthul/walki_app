df2572ac3f56e6d32a86b4a6f257c8bc36a763f8
nazarii <nazarii.honcharenko@haven.com>
2024-07-07 13:57:50 +0300

    Add PointOfInterestService in trip_repository.dart

--- packages/trip_repository/lib/src/trip_repository.dart
+++ packages/trip_repository/lib/src/trip_repository.dart
@@ -1,1 +1,8 @@
-class TripRepository {}
+import 'package:point_of_interest_service/point_of_interest_service.dart';
+
+class TripRepository {
+  TripRepository({required PointOfInterestService pointOfInterestService})
+      : _pointOfInterestService = pointOfInterestService;
+
+  final PointOfInterestService _pointOfInterestService;
+}
