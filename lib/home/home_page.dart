eff981a8be7dbd2b10bea5f284eedda86470d9f4
nazarii <nazarii.honcharenko@haven.com>
2024-07-07 16:02:21 +0300

    Add HomeCubit to home_page.dart

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vall/home/cubit/home_cubit.dart';
 import 'package:vall/home/trip/trip_component.dart';
 import 'package:vall/home/trip_settings/trip_settings_component.dart';
 
@@ -7,17 +9,22 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) => const AnnotatedRegion<SystemUiOverlayStyle>(
+  Widget build(BuildContext context) => AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.dark,
         child: Scaffold(
           backgroundColor: Colors.white,
-          body: SafeArea(
-            top: false,
-            child: Stack(children: [
-              TripComponent(),
-              TripSettingsComponent(),
-            ]),
-          ),
+          body: BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
+            if (state is InitialLocationLoaded) {
+              return SafeArea(
+                top: false,
+                child: Stack(children: [
+                  TripComponent(initialLocation: state.location),
+                  const TripSettingsComponent(),
+                ]),
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          }),
         ),
       );
 }
