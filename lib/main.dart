3352e9ad2dc870f571ae4c17699069870db860e7
nazarii <nazarii.honcharenko@haven.com>
2024-07-14 19:24:52 +0300

    Setup localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vall/app/app_dependencies.dart';
 import 'package:vall/app/routes/app_routes.dart';
+import 'package:vall/l10n/generated/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,8 @@ class MyApp extends StatelessWidget {
         child: MaterialApp.router(
           debugShowCheckedModeBanner: false,
           title: 'Vall',
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
