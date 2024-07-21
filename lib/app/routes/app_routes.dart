e435c586f6212e13badff6ac1d29ef829fa758cc
nazarii <nazarii.honcharenko@haven.com>
2024-07-21 14:48:19 +0300

    Add StatefulShellRoute in app_routes.dart

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,14 +1,30 @@
 import 'package:go_router/go_router.dart';
+import 'package:vall/app/common/component/app_bottom_navigation_bar.dart';
 import 'package:vall/home/home_page.dart';
+import 'package:vall/home/places/places_page.dart';
 
-const String _initialPagePath = '/';
+const String _tripPagePath = '/trip';
+const String _placesPagePath = '/places';
 
 final appRoutes = GoRouter(
-  initialLocation: _initialPagePath,
+  initialLocation: _tripPagePath,
   routes: [
-    GoRoute(
-      path: _initialPagePath,
-      builder: (context, state) => const HomePage(),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) => AppBottomNavigationBar(navigationShell: navigationShell),
+      branches: [
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: _tripPagePath,
+            builder: (context, state) => const HomePage(),
+          ),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: _placesPagePath,
+            builder: (context, state) => const PlacesPage(),
+          ),
+        ]),
+      ],
     ),
   ],
 );
---
+++ lib/home/places/places_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PlacesPage extends StatelessWidget {
+  const PlacesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Placeholder();
+}
