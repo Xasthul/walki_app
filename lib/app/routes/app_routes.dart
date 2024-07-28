c028507f50823df2e8667ac1c58bab89b7f9528a
nazarii <nazarii.honcharenko@haven.com>
2024-07-28 23:55:59 +0300

    Update app_routes.dart

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,37 +1,29 @@
 import 'package:go_router/go_router.dart';
-import 'package:vall/app/common/component/app_bottom_navigation_bar.dart';
 import 'package:vall/home/home_page.dart';
 import 'package:vall/home/places/places_page.dart';
 import 'package:vall/home/trip/trip_page.dart';
 
-const String _homePagePath = '/home';
-const String _tripPagePath = 'trip';
-const String _placesPagePath = 'places';
+const String _tripPagePath = '/trip';
+const String _placesPagePath = '/places';
 
 final appRoutes = GoRouter(
-  initialLocation: _homePagePath,
+  initialLocation: _tripPagePath,
   routes: [
-    GoRoute(
-      path: _homePagePath,
-      builder: (context, state) => const HomePage(),
-      routes: [
-        StatefulShellRoute.indexedStack(
-          builder: (context, state, navigationShell) => AppBottomNavigationBar(navigationShell: navigationShell),
-          branches: [
-            StatefulShellBranch(routes: [
-              GoRoute(
-                path: _tripPagePath,
-                builder: (context, state) => const TripPage(),
-              ),
-            ]),
-            StatefulShellBranch(routes: [
-              GoRoute(
-                path: _placesPagePath,
-                builder: (context, state) => const PlacesPage(),
-              ),
-            ]),
-          ],
-        ),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) => HomePage(navigationShell: navigationShell),
+      branches: [
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: _tripPagePath,
+            builder: (context, state) => const TripPage(),
+          ),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: _placesPagePath,
+            builder: (context, state) => const PlacesPage(),
+          ),
+        ]),
       ],
     ),
   ],
