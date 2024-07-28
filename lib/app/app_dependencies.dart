7772e5f9314d3524f8104a2f2abcacbec89cdb4b
nazarii <nazarii.honcharenko@haven.com>
2024-07-28 23:55:12 +0300

    Remove all providers from app_dependencies.dart

--- lib/app/app_dependencies.dart
+++ lib/app/app_dependencies.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:vall/home/trip/common/service/point_of_interest_service.dart';
-import 'package:vall/home/trip/common/use_case/trip_use_case.dart';
 
 class AppDependencies extends StatelessWidget {
   const AppDependencies({
@@ -11,16 +8,7 @@ class AppDependencies extends StatelessWidget {
 
   final Widget _child;
 
+  // TODO(naz): add app dependencies
   @override
-  Widget build(BuildContext context) => MultiRepositoryProvider(
-        providers: [
-          RepositoryProvider(create: (_) => PointOfInterestService()),
-          RepositoryProvider(
-            create: (context) => TripUseCase(
-              pointOfInterestService: RepositoryProvider.of<PointOfInterestService>(context),
-            ),
-          ),
-        ],
-        child: _child,
-      );
+  Widget build(BuildContext context) => _child;
 }
