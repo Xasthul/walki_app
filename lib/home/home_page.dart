2170f5bf751e0e315d2c0a1524a8ce1fac6081a3
nazarii <nazarii.honcharenko@haven.com>
2024-07-28 23:55:52 +0300

    Remove app_bottom_navigation_bar.dart and move its content into home_page.dart

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -5,22 +5,57 @@ import 'package:vall/home/cubit/home_cubit.dart';
 import 'package:vall/home/home_dependencies.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({
+    super.key,
+    required StatefulNavigationShell navigationShell,
+  }) : _navigationShell = navigationShell;
+
+  final StatefulNavigationShell _navigationShell;
 
   @override
   Widget build(BuildContext context) => HomeDependencies(
-        child: Scaffold(
-          // TODO(naz): use themes
-          backgroundColor: Colors.white,
-          body: BlocConsumer<HomeCubit, HomeState>(
-            listener: (context, state) {
-              if (state is HomeGetLocationPermissionSuccess) {
-                // TODO(naz): use type routes
-                return context.go('/home/trip');
-              }
-            },
-            builder: (context, state) => const Center(child: CircularProgressIndicator()),
+        child: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+            if (state is! HomeGetLocationPermissionSuccess) {
+              return const _HomeLoadingContentComponent();
+            }
+            return _HomeContentComponent(navigationShell: _navigationShell);
+          },
+        ),
+      );
+}
+
+class _HomeContentComponent extends StatelessWidget {
+  const _HomeContentComponent({
+    required StatefulNavigationShell navigationShell,
+  }) : _navigationShell = navigationShell;
+
+  final StatefulNavigationShell _navigationShell;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.white,
+        body: _navigationShell,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _navigationShell.currentIndex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.map_rounded), label: 'Trip'),
+            BottomNavigationBarItem(icon: Icon(Icons.place_rounded), label: 'Places'),
+          ],
+          onTap: (index) => _navigationShell.goBranch(
+            index,
+            initialLocation: index == _navigationShell.currentIndex,
           ),
         ),
+      );
+}
+
+class _HomeLoadingContentComponent extends StatelessWidget {
+  const _HomeLoadingContentComponent();
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(child: CircularProgressIndicator()),
       );
 }
