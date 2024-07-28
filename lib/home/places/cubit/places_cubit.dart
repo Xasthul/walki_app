959fc00dec6e02e702f2ea1b45b02e1cf80111ee
nazarii <nazarii.honcharenko@haven.com>
2024-07-28 16:40:47 +0300

    Add places_cubit.dart

---
+++ lib/home/places/cubit/places_cubit.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vall/home/trip/common/entity/trip_step.dart';
+import 'package:vall/home/trip/common/use_case/trip_use_case.dart';
+
+part 'places_state.dart';
+
+class PlacesCubit extends Cubit<PlacesState> {
+  PlacesCubit({
+    required TripUseCase tripUseCase,
+  })  : _tripUseCase = tripUseCase,
+        super(PlacesNoTripCreated()) {
+    _init();
+  }
+
+  final TripUseCase _tripUseCase;
+  StreamSubscription<List<TripStep>>? _tripSubscription;
+
+  void _init() => _tripSubscription = _tripUseCase.trip.listen((trip) {
+        if (trip.isEmpty) {
+          return emit(PlacesNoTripCreated());
+        }
+        emit(PlacesTripCreated());
+      });
+
+  @override
+  Future<void> close() {
+    _tripSubscription?.cancel();
+    return super.close();
+  }
+}
