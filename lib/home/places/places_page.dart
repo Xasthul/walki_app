25f5b28df2aad913a041264e963f036ac076c19d
nazarii <nazarii.honcharenko@haven.com>
2024-07-29 23:40:53 +0300

    Add _PlacesNoTripCreatedComponent in places_page.dart

--- lib/home/places/places_page.dart
+++ lib/home/places/places_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:vall/home/places/cubit/places_cubit.dart';
 
 class PlacesPage extends StatelessWidget {
@@ -10,10 +11,27 @@ class PlacesPage extends StatelessWidget {
         body: SafeArea(
           child: BlocBuilder<PlacesCubit, PlacesState>(
             builder: (context, state) => switch (state) {
-              PlacesNoTripCreated() => const Center(child: Text('No trip created yet')),
+              PlacesNoTripCreated() => const _PlacesNoTripCreatedComponent(),
               PlacesTripCreated() => const Center(child: Text('The trip is created'))
             },
           ),
         ),
+      );
+}
+
+class _PlacesNoTripCreatedComponent extends StatelessWidget {
+  const _PlacesNoTripCreatedComponent();
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text('No trip created yet'),
+          const SizedBox(height: 24),
+          FilledButton(
+            // TODO(naz): use types routes
+            onPressed: () => context.go('/trip'),
+            child: const Text('Go to trip creation'),
+          ),
+        ]),
       );
 }
