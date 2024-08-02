4d90d978b72e27a0a4821e0ed1b201fcccea09c3
nazarii <nazarii.honcharenko@haven.com>
2024-08-02 23:03:49 +0300

    Add Equatable in point_of_interest.dart

--- lib/home/trip/common/entity/point_of_interest.dart
+++ lib/home/trip/common/entity/point_of_interest.dart
@@ -1,9 +1,20 @@
-class PointOfInterest {
-  PointOfInterest(
-    this.latitude,
-    this.longitude,
-  );
+import 'package:equatable/equatable.dart';
 
+class PointOfInterest extends Equatable {
+  const PointOfInterest({
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  final String name;
   final double latitude;
   final double longitude;
+
+  @override
+  List<Object> get props => [
+        name,
+        latitude,
+        longitude,
+      ];
 }
