3b7b07d44d21436800dea587cfc2bc191038f78e
nazarii <nazarii.honcharenko@haven.com>
2024-08-05 00:28:40 +0300

    Add _PlacesDiscoveredComponent in places_page.dart

--- lib/home/places/places_page.dart
+++ lib/home/places/places_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:vall/home/places/cubit/places_cubit.dart';
+import 'package:vall/home/trip/common/entity/point_of_interest.dart';
 
 class PlacesPage extends StatelessWidget {
   const PlacesPage({super.key});
@@ -11,27 +12,53 @@ class PlacesPage extends StatelessWidget {
         body: SafeArea(
           child: BlocBuilder<PlacesCubit, PlacesState>(
             builder: (context, state) => switch (state) {
-              PlacesNoTripCreated() => const _PlacesNoTripCreatedComponent(),
-              PlacesTripCreated() => const Center(child: Text('The trip is created'))
+              PlacesNotDiscovered() => const _PlacesNotDiscoveredComponent(),
+              PlacesDiscovered() => _PlacesDiscoveredComponent(places: state.places),
             },
           ),
         ),
       );
 }
 
-class _PlacesNoTripCreatedComponent extends StatelessWidget {
-  const _PlacesNoTripCreatedComponent();
+class _PlacesNotDiscoveredComponent extends StatelessWidget {
+  const _PlacesNotDiscoveredComponent();
 
   @override
   Widget build(BuildContext context) => Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Text('No trip created yet'),
+          const Text('No places found yet'),
           const SizedBox(height: 24),
           FilledButton(
             // TODO(naz): use types routes
             onPressed: () => context.go('/trip'),
-            child: const Text('Go to trip creation'),
+            child: const Text('Go to places discovery'),
           ),
         ]),
       );
+}
+
+class _PlacesDiscoveredComponent extends StatelessWidget {
+  const _PlacesDiscoveredComponent({
+    required List<PointOfInterest> places,
+  }) : _places = places;
+
+  final List<PointOfInterest> _places;
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = BlocProvider.of<PlacesCubit>(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: ListView.builder(
+        itemCount: _places.length,
+        itemBuilder: (context, index) => Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(_places[index].name),
+          TextButton(
+            onPressed: () => cubit.addPlaceToTrip(_places[index]),
+            child: const Text('Add to trip'),
+          ),
+        ]),
+      ),
+    );
+  }
 }
