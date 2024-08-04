e5749661041908672f83f8c007c3cb90c2c939a9
nazarii <nazarii.honcharenko@haven.com>
2024-08-05 00:28:27 +0300

    Add addPlaceToTrip in places_cubit.dart

--- lib/home/places/cubit/places_cubit.dart
+++ lib/home/places/cubit/places_cubit.dart
@@ -2,32 +2,42 @@ import 'dart:async';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:vall/home/trip/common/entity/trip.dart';
+import 'package:vall/home/places/common/repository/places_repository.dart';
+import 'package:vall/home/trip/common/entity/point_of_interest.dart';
 import 'package:vall/home/trip/common/repository/trip_repository.dart';
 
 part 'places_state.dart';
 
 class PlacesCubit extends Cubit<PlacesState> {
   PlacesCubit({
+    required PlacesRepository placesRepository,
     required TripRepository tripRepository,
-  })  : _tripRepository = tripRepository,
-        super(PlacesNoTripCreated()) {
+  })  : _placesRepository = placesRepository,
+        _tripRepository = tripRepository,
+        super(PlacesNotDiscovered()) {
     _init();
   }
 
+  final PlacesRepository _placesRepository;
   final TripRepository _tripRepository;
-  StreamSubscription<Trip?>? _tripSubscription;
+  StreamSubscription<List<PointOfInterest>>? _placesSubscription;
 
-  void _init() => _tripSubscription = _tripRepository.trip.listen((trip) {
-        if (trip == null) {
-          return emit(PlacesNoTripCreated());
+  void _init() => _placesSubscription = _placesRepository.places.listen((places) {
+        if (places.isEmpty) {
+          return emit(PlacesNotDiscovered());
         }
-        emit(PlacesTripCreated());
+        emit(PlacesDiscovered(places));
       });
+
+  // TODO(naz): find optimal route method to sort selected places
+
+  void addPlaceToTrip(PointOfInterest place) {
+    _tripRepository.addPlace(place);
+  }
 
   @override
   Future<void> close() {
-    _tripSubscription?.cancel();
+    _placesSubscription?.cancel();
     return super.close();
   }
 }
