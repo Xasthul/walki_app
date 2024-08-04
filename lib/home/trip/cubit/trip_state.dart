b0f67723c4cc208333e0b91ab96033d3a9fb31b2
nazarii <nazarii.honcharenko@haven.com>
2024-08-05 00:23:25 +0300

    Add TripCreation and TripPlacesNearbyFound in trip_state.dart

--- lib/home/trip/cubit/trip_state.dart
+++ lib/home/trip/cubit/trip_state.dart
@@ -13,20 +13,46 @@ class TripInitial extends TripState {}
 
 class TripLoading extends TripState {}
 
+class TripPlacesNearbyFound extends TripState {
+  const TripPlacesNearbyFound({required this.places});
+
+  final List<LatLng> places;
+
+  @override
+  List<Object?> get props => super.props..add(places);
+}
+
+class TripCreation extends TripState {
+  const TripCreation({
+    required this.places,
+    required this.selectedPlaces,
+  });
+
+  final List<LatLng> places;
+  final List<LatLng> selectedPlaces;
+
+  @override
+  List<Object?> get props => super.props
+    ..addAll([
+      places,
+      selectedPlaces,
+    ]);
+}
+
 class TripCreated extends TripState {
   const TripCreated({
     required this.polylinePoints,
-    required this.tripSteps,
+    required this.places,
   });
 
   final List<LatLng> polylinePoints;
-  final List<LatLng> tripSteps;
+  final List<LatLng> places;
 
   @override
   List<Object?> get props => super.props
     ..addAll([
       polylinePoints,
-      tripSteps,
+      places,
     ]);
 }
 
