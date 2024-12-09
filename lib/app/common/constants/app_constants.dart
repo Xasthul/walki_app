4366c63699b7f4a6f5d90c144e17cdda8faca070
nazarii <nazarii.honcharenko@haven.com>
2024-12-09 10:56:45 +0200

    Increase visit place threshold

--- lib/app/common/constants/app_constants.dart
+++ lib/app/common/constants/app_constants.dart
@@ -1,4 +1,5 @@
 abstract class AppConstants {
   static const String serviceBaseUrl = 'http://localhost:8080';
   static const String googleApiKey = 'AIzaSyACa7YoZzXI4j-KRlKJkxk2kdWclT7cGM8';
+  static const visitPlaceThreshold = 100; // in meters
 }
